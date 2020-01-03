8a8283e3be98e49e6f2b211cc60c71d3659ea981
Ihor <ihor.sasovets@gmail.com>
2020-01-03 16:48:04 +0200

    - added logs

--- tests/material.spec.js
+++ tests/material.spec.js
@@ -8,7 +8,11 @@ describe('Angular material suite', () => {
       const openModalButton = $('ol .mat-raised-button');
       await loginPage.clickOnElement(openModalButton);
       const nameField = $('.mat-dialog-content input');
+      // Just to visualize
+      await browser.pause(3000);
       await loginPage.changeElementText(nameField, 'Dealete');
+      // Just to visualize
+      await browser.pause(3000);
       const modalConfirmButton = $$('.mat-dialog-actions button').then(el => el[1]);
       await loginPage.clickOnElement(modalConfirmButton);
       const textField = $('li i');
@@ -16,13 +20,31 @@ describe('Angular material suite', () => {
       const elementText = await textField.then(el => el.getText());
       assert.equal(elementText, 'Dealete');
     });
+    it('Positive scenario #2', async() => {
+      await loginPage.navigateTo('https://angular-cbpbdt.stackblitz.io/');
+      const openModalButton = $('ol .mat-raised-button');
+      await loginPage.clickOnElement(openModalButton);
+      const nameField = $('.mat-dialog-content input');
+      // Just to visualize
+      await browser.pause(3000);
+      await loginPage.changeElementText(nameField, 'Accent');
+      // Just to visualize
+      await browser.pause(3000);
+      const modalConfirmButton = $$('.mat-dialog-actions button').then(el => el[1]);
+      await loginPage.clickOnElement(modalConfirmButton);
+      const textField = $('li i');
+      await loginPage.waitForElement(textField, {maxRetries: 3});
+      const elementText = await textField.then(el => el.getText());
+      assert.equal(elementText, 'Accent');
+    });
     it('Should confirm the bug', async() => {
       await loginPage.navigateTo('https://angular-cbpbdt.stackblitz.io/');
       const openModalButton = $('ol .mat-raised-button');
       await loginPage.clickOnElement(openModalButton);
       const nameField = $('.mat-dialog-content input');
-      await browser.pause(4000);
       await loginPage.changeElementText(nameField, 'Delete');
+      // Just to visualize
+      await browser.pause(3000);
       const modalConfirmButton = $$('.mat-dialog-actions button').then(el => el[1]);
       await loginPage.clickOnElement(modalConfirmButton);
       const textField = $('li i');
